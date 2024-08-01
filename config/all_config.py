4249341b7b41bb75c1d56965f362522587315a59
saleemhamo <saleemhamo@gmail.com>
2024-08-01 12:25:40 +0300

    file formating all_config.py

--- config/all_config.py
+++ config/all_config.py
@@ -8,12 +8,13 @@ import numpy as np
 import datetime
 import logging
 
+
 def gen_log(model_path, msg, log_name):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
 
-    log_file = model_path + '/'+ log_name + '.txt'
+    log_file = model_path + '/' + log_name + '.txt'
     fh = logging.FileHandler(log_file, mode='a')
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
@@ -28,6 +29,7 @@ def gen_log(model_path, msg, log_name):
     logger.removeHandler(fh)
     logger.removeHandler(ch)
     # return logger
+
 
 class AllConfig(Config):
     def __init__(self):
@@ -46,7 +48,7 @@ class AllConfig(Config):
     def parse_args(self):
         description = 'Text-to-Video Retrieval'
         parser = argparse.ArgumentParser(description=description)
-        
+
         # data parameters
         parser.add_argument('--dataset_name', type=str, default='MSRVTT', help="Dataset name")
         parser.add_argument('--videos_dir', type=str, default='data/MSRVTT/vids', help="Location of videos")
@@ -68,7 +70,8 @@ class AllConfig(Config):
         # model parameters
         # parser.add_argument('--huggingface', action='store_true', default=False)
         parser.add_argument('--arch', type=str, default='clip_transformer')
-        parser.add_argument('--clip_arch', type=str, default='ViT-B/32', choices=['ViT-B/32', 'ViT-B/16'], help="CLIP arch. only when not using huggingface")
+        parser.add_argument('--clip_arch', type=str, default='ViT-B/32', choices=['ViT-B/32', 'ViT-B/16'],
+                            help="CLIP arch. only when not using huggingface")
         parser.add_argument('--embed_dim', type=int, default=512, help="Dimensionality of the model embedding")
 
         # training parameters
@@ -78,14 +81,18 @@ class AllConfig(Config):
         parser.add_argument('--batch_size', type=int, default=32)
         parser.add_argument('--num_epochs', type=int, default=5)
         parser.add_argument('--weight_decay', type=float, default=0.2, help='Weight decay')
-        parser.add_argument('--warmup_proportion', type=float, default=0.1, help='Warmup proportion for learning rate schedule')
+        parser.add_argument('--warmup_proportion', type=float, default=0.1,
+                            help='Warmup proportion for learning rate schedule')
 
         # frame pooling parameters
         parser.add_argument('--pooling_type', type=str)
         parser.add_argument('--k', type=int, default=-1, help='K value for topk pooling')
-        parser.add_argument('--attention_temperature', type=float, default=0.01, help='Temperature for softmax (used in attention pooling only)')
-        parser.add_argument('--num_mha_heads', type=int, default=1, help='Number of parallel heads in multi-headed attention')
-        parser.add_argument('--transformer_dropout', type=float, default=0.3, help='Dropout prob. in the transformer pooling')
+        parser.add_argument('--attention_temperature', type=float, default=0.01,
+                            help='Temperature for softmax (used in attention pooling only)')
+        parser.add_argument('--num_mha_heads', type=int, default=1,
+                            help='Number of parallel heads in multi-headed attention')
+        parser.add_argument('--transformer_dropout', type=float, default=0.3,
+                            help='Dropout prob. in the transformer pooling')
 
         # system parameters
         parser.add_argument('--num_workers', type=int, default=8)
@@ -94,19 +101,30 @@ class AllConfig(Config):
         parser.add_argument('--tb_log_dir', type=str, default='logs')
 
         # @WJM:
-        parser.add_argument("--datetime",type=str,default=None,help='to be specificed for loading pre-trained checkpoint ')
-        parser.add_argument('--stochasic_trials', type=int, default=20, help='perform [stochastic trials] to compute the averaged text embedding at validation')
+        parser.add_argument("--datetime", type=str, default=None,
+                            help='to be specificed for loading pre-trained checkpoint ')
+        parser.add_argument('--stochasic_trials', type=int, default=20,
+                            help='perform [stochastic trials] to compute the averaged text embedding at validation')
         parser.add_argument('--gpu', type=str, default=None, help="gpu id")
-        parser.add_argument('--support_loss_weight',  type=float, default=0.0, help='compute the contrastive between pooled-video and support text embedding, default=0.')
-        parser.add_argument('--batch_size_split', type=int, default=None, help="split integer for batch-wise bmm, larger to save more memory. Default=None, automatically split into 10-sample batches")
-        parser.add_argument('--chunk_size', type=int, default=128, help="split integer for batch-wise torch.norm, larger to save more memory. Default=128")
-        parser.add_argument('--noloss_record', action='store_true', default=False, help='if specified, no loss values will be recorded to speed up training')
-        parser.add_argument('--save_memory_mode', action='store_true', default=False, help='if specified, will use sim_matrix_inference_stochastic_light_allops() at eval no matter of the dataset')
-        parser.add_argument('--raw_video', action='store_true', default=False, help='For Charades dataest. if specified, will load video format of .mp4')
-        parser.add_argument('--skip_eval', action='store_true', default=False, help='If specified, will not conduct validation() per epoch but only save ckpts')
+        parser.add_argument('--support_loss_weight', type=float, default=0.0,
+                            help='compute the contrastive between pooled-video and support text embedding, default=0.')
+        parser.add_argument('--batch_size_split', type=int, default=None,
+                            help="split integer for batch-wise bmm, larger to save more memory. Default=None, automatically split into 10-sample batches")
+        parser.add_argument('--chunk_size', type=int, default=128,
+                            help="split integer for batch-wise torch.norm, larger to save more memory. Default=128")
+        parser.add_argument('--noloss_record', action='store_true', default=False,
+                            help='if specified, no loss values will be recorded to speed up training')
+        parser.add_argument('--save_memory_mode', action='store_true', default=False,
+                            help='if specified, will use sim_matrix_inference_stochastic_light_allops() at eval no matter of the dataset')
+        parser.add_argument('--raw_video', action='store_true', default=False,
+                            help='For Charades dataest. if specified, will load video format of .mp4')
+        parser.add_argument('--skip_eval', action='store_true', default=False,
+                            help='If specified, will not conduct validation() per epoch but only save ckpts')
         parser.add_argument('--DSL', action='store_true', default=False, help='If specified, will normalize use DSL')
-        parser.add_argument('--stochastic_prior', type=str, default='uniform01', choices=['uniform01', 'normal'], help="use which prior for the re-parameterization, default to unifrom01")
-        parser.add_argument('--stochastic_prior_std',  type=float, default=1.0, help='std value for the reprameterization prior')
+        parser.add_argument('--stochastic_prior', type=str, default='uniform01', choices=['uniform01', 'normal'],
+                            help="use which prior for the re-parameterization, default to unifrom01")
+        parser.add_argument('--stochastic_prior_std', type=float, default=1.0,
+                            help='std value for the reprameterization prior')
 
         args = parser.parse_args()
 
